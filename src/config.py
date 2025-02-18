d12c2be121adcc7bdc2366ef135949c13d57f6ee
Haebin Lee <hblee8080@gmail.com>
2025-02-18 11:00:43 -0800

    feat: add default config parameteres

--- src/config.py
+++ src/config.py
@@ -3,16 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:testpass@localhost:5432/bookly"
+    JWT_SECRET: str = "2f1b750aea00acb11fe974de577c037f2f5da569a5dc9f7457cdcd8d8878e521"
+    JWT_ALGORITHM: str = "HS256"
     REDIS_URL: str = "redis://localhost:6379/0"
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-    MAIL_PORT: int
-    MAIL_SERVER: str
-    MAIL_FROM_NAME: str
+    REDIS_HOST: str = "redis"
+    REDIS_PORT: int = 6379
+    MAIL_USERNAME: str = "norma.schumm5@ethereal.email"
+    MAIL_PASSWORD: str = "3UeGDK6pasgKZMQjFE"
+    MAIL_FROM: str = "hblee8080@gmail.com"
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.ethereal.email"
+    MAIL_FROM_NAME: str = "Haebin Lee"
     MAIL_STARTTLS: bool = True
     MAIL_SSL_TLS: bool = False
     USE_CREDENTIALS: bool = True
